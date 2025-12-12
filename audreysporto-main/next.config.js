ea6b6f3b85d3c9d279464ce4b7744e471b20700a
authcar@gmail.com <youremail@example.com>
2025-12-12 09:04:12 +0700

    build

--- audreysporto-main/next.config.js
+++ audreysporto-main/next.config.js
@@ -1,11 +1,19 @@
-module.exports = {
-    async redirects() {
-        return [
-          {
-            source: '/blog',
-            destination: '/',
-            permanent: true,
-          },
-        ]
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", 
+  images: {
+    unoptimized: true, 
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/",
+        permanent: true,
       },
+    ];
+  },
 };
+
+module.exports = nextConfig;
